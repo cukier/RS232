8b5963ad183ddf1fc924a0b20cca53c21cfeb7df
cuki <mauricio@krebs.com.br>
2013-06-04 09:11:45 -0300

    original funcionando

---
+++ Debug/main.d
@@ -0,0 +1,3 @@
+main.d main.o: ../main.c ../rs232.h
+
+../rs232.h:
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/rs232.c 
+
+OBJS += \
+./src/rs232.o 
+
+C_DEPS += \
+./src/rs232.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C Compiler'
+	gcc -I"E:\Eletronica\workspace\RS232\src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
